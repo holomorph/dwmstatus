8ba13816682add33d2baa6ff1b6d4bae0aaa1a37
holomorph <mvo@localhost.localdomain>
2012-11-27 01:05:35 -0500

    changed colors

--- dwmstatus.h
+++ dwmstatus.h
@@ -34,20 +34,20 @@
 #define VOL_MUTE_STR "\x03Ô -%ddB \x09Ý " // muted
 
 // Core load, temperature, and memory usage
-#define CPU_STR      "\x01Ï %02.f %02.f %02.f  " // cpu load
+#define CPU_STR      "\x0DÏ %02.f %02.f %02.f  " // cpu load
 #define CPU_HI_STR   "\x03Ï %02.f %02.f %02.f  " // cpu load urgent > CPU_HI
-#define TEMP_STR     "\x01Ç %dC  "          // core temp (C)
+#define TEMP_STR     "\x0DÇ %dC  "          // core temp (C)
 #define TEMP_HI_STR  "\x03Ç %dC  "          // core temp urgent > CORETEMP_HI
-#define MEM_STR      "\x01Þ %dM \x09Ý\x01 " // memory usage
+#define MEM_STR      "\x0DÞ %dM \x09Ý\x01 " // memory usage
 
 // Wireless usage (wlan0)
 #define WIFI_STR     "\x07Ð %-4s \x06Ñ %-3s \x09Ý\x01 " // wlan0 down/up rates
 
 // Battery status, level, time remaining
-#define BAT_STR      "\x08Á %d%% (%02.2f) " // battery discharging & time left
+#define BAT_STR      "\x0AÁ %d%% (%02.2f) " // battery discharging & time left
 #define BAT_LOW_STR  "\x03Á %d%% (%02.2f) " // " urgent if below BAT_LOW
-#define BAT_CHRG_STR "\x08Å Á %d%% (%d:%02.0f) " // " charging
-#define BAT_FULL_STR "\x08À Á %d%% "             // " full
+#define BAT_CHRG_STR "\x0AÅ %d%% (%d:%02.0f) " // " charging
+#define BAT_FULL_STR "\x0AÀ %d%% "             // " full
 
 // Date-time
 #define DATE_TIME_STR "\x02 Õ %a %d %b %H:%M "    // date-time
