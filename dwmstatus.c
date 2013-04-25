4a000c70bbadc6049d599d2e035031feafb61e59
Mark Oteiza <mvoteiza@udel.edu>
2013-04-25 00:43:09 -0400

    suckless barebones v1.2

--- dwmstatus.c
+++ dwmstatus.c
@@ -1,82 +1,129 @@
-#include <stdlib.h>
-#include <stdio.h>
+#define _BSD_SOURCE
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
+#include <strings.h>
+#include <sys/time.h>
 #include <time.h>
-#include <mpd/client.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #include <X11/Xlib.h>
 
-#include "config.h"
-#include "function.h"
+char *tzargentina = "America/Buenos_Aires";
+char *tzutc = "UTC";
+char *tzberlin = "Europe/Berlin";
 
-int main() {
-  char mpd[50];
-  char volume[15];
-  char load[22];
-  char mem[20];
-  char coretemp[11];
-  char wifi[30];
-  char battery[37];
-  char datetime[24];
+static Display *dpy;
 
-  char status[200];
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
 
-  // Evaluate initial wireless statistics
-  FILE *f;
-  f = fopen(WIFI_DN,"r");
-  fscanf(f,"%ld",&rx_old); fclose(f);
-  f = fopen(WIFI_UP,"r");
-  fscanf(f,"%ld",&tx_old); fclose(f);
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
 
-  if (!(dpy = XOpenDisplay(NULL))) {
-    fprintf(stderr, "dwmstatus: could not open display.\n");
-    return 1;
-  }
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
 
-  for (;;sleep(INTERVAL)) {
-    // reset/clear the status
-    status[0]='\0';
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
 
-    // Music player daemon
-    print_mpd(mpd);
-    strcat(status,mpd);
-
-    // Audio volume:
-    print_volume(volume);
-    strcat(status,volume);
-
-    // Core load averages
-    print_load(load);
-    strcat(status,load);
-
-    // Core temperature
-    print_coretemp(coretemp);
-    strcat(status,coretemp);
-
-    // Memory use:
-    print_memory(mem);
-    strcat(status,mem);
-
-    // Wireless network usage
-    print_wifi(wifi);
-    strcat(status,wifi);
-    // overwrite old values with new ones
-    rx_old = rx_new;
-    tx_old = tx_new;
-
-    // battery!!!!!!
-    print_battery(battery);
-    strcat(status,battery);
-
-    // date time!!
-    print_datetime(datetime);
-    strcat(status,datetime);
-
-    // Set root name
-    setstatus(status);
-  }
-
-  free(status);
-  XCloseDisplay(dpy);
-  return 0;
+	return ret;
 }
+
+void
+settz(char *tzname)
+{
+	setenv("TZ", tzname, 1);
+}
+
+char *
+mktimes(char *fmt, char *tzname)
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	memset(buf, 0, sizeof(buf));
+	settz(tzname);
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (timtm == NULL) {
+		perror("localtime");
+		exit(1);
+	}
+
+	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		exit(1);
+	}
+
+	return smprintf("%s", buf);
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+char *
+loadavg(void)
+{
+	double avgs[3];
+
+	if (getloadavg(avgs, 3) < 0) {
+		perror("getloadavg");
+		exit(1);
+	}
+
+	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+}
+
+int
+main(void)
+{
+	char *status;
+	char *avgs;
+	char *tmar;
+	char *tmutc;
+	char *tmbln;
+
+	if (!(dpy = XOpenDisplay(NULL))) {
+		fprintf(stderr, "dwmstatus: cannot open display.\n");
+		return 1;
+	}
+
+	for (;;sleep(90)) {
+		avgs = loadavg();
+		tmar = mktimes("%H:%M", tzargentina);
+		tmutc = mktimes("%H:%M", tzutc);
+		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
+
+		status = smprintf("L:%s A:%s U:%s %s",
+				avgs, tmar, tmutc, tmbln);
+		setstatus(status);
+		free(avgs);
+		free(tmar);
+		free(tmutc);
+		free(tmbln);
+		free(status);
+	}
+
+	XCloseDisplay(dpy);
+
+	return 0;
+}
+
