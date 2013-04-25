461173ecc4ca259f6ecdf7347bbd214c12a11ae1
Mark Oteiza <mvoteiza@udel.edu>
2013-04-25 01:06:42 -0400

    modularize

---
+++ function.h
@@ -0,0 +1,75 @@
+static Display *dpy;
+
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
+
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
+
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
+
+	return ret;
+}
+
+void
+settz(char *tzname)
+{
+	setenv("TZ", tzname, 1);
+}
+
+char *
+mktimes(char *fmt, char *tzname)
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	memset(buf, 0, sizeof(buf));
+	settz(tzname);
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (timtm == NULL) {
+		perror("localtime");
+		exit(1);
+	}
+
+	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		exit(1);
+	}
+
+	return smprintf("%s", buf);
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+char *
+loadavg(void)
+{
+	double avgs[3];
+
+	if (getloadavg(avgs, 3) < 0) {
+		perror("getloadavg");
+		exit(1);
+	}
+
+	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+}
